d76be3094811f6557090006b905fa1074d1f533e
tobiasdr <tobias.drees@gmail.com>
2019-03-03 22:03:14 +0000

    minor changes

--- record.py
+++ record.py
@@ -5,25 +5,19 @@ import time
 #initialise serial port S0
 serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=True)
 
-ids = []
 #scanning daisy chain and storing ID numbers in array
-worked = False
-while not worked:
-    try:
-        ids = serial_connection.scan()
-        worked = True
-    except:
-        pass
+ids = serial_connection.scan()
 
 #disable torque for servos
 for id in ids:
     serial_connection.send(InstructionPacket(id, 0x03, bytes([0x18, 0x00])))
 
 #initialise position array
-position = [[None for x in range(0)] for y in range(7)]
+position = [[None for x in range(0)] for y in range(len(ids))]
+print(position)
+#initialise time
+start = time.time()
 
-
-#function that ignores errors when moving
 def sure_goto(id, pos, sp):
     max_retries = 10
     retry = 0
@@ -33,29 +27,27 @@ def sure_goto(id, pos, sp):
             break
         except:
             pass
-    print("Retries: {}".format(retry))
+
 
 try:
     while True:
         for id in ids:
             curr_pos = serial_connection.get_present_position(id)
-            position[id].append(curr_pos)
-            print(position[id])
+            position[id-1].append(curr_pos)
+            print(position[id-1])
             time.sleep(0.05)
 
 except KeyboardInterrupt:
     pass
 
 for id in ids:
-    sure_goto(id, position[id][0], 200)
-    time.sleep(0.08)
+    sure_goto(id, position[id-1][0], 200)
 
 time.sleep(1)
 
-for i in range(len(position[id])):
-    for id in ids:
-        sure_goto(id, position[id][i], 350)
+for id in ids:
+    for i in range(len(position[1])):
+        sure_goto(id, position[id-1][i], 200)
         time.sleep(0.08)
 
-serial_connection.close()
 print("finished")
