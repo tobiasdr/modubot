71df7b01c27272ee7184daacd97012f028764d7a
tobiasdr <tobias.drees@gmail.com>
2019-03-03 20:12:53 +0000

    scalable daisy chain code

--- daisytest.py
+++ daisytest.py
@@ -9,9 +9,7 @@ serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=Tru
 # Ping the dynamixel unit(s)
 ids_available = serial_connection.scan()
 
-for dynamixel_id in ids_available:
-    print(dynamixel_id)
-
+print(ids_available[0])
 # Close the serial connection
 serial_connection.close()
 gpio.cleanup()
--- recordtest.py
+++ recordtest.py
@@ -5,17 +5,48 @@ import time
 #initialise serial port S0
 serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=True)
 
-#disable torque for servo with ID 1
-serial_connection.send(InstructionPacket(1, 0x03, bytes([0x18, 0x00])))
+#scanning daisy chain and storing ID numbers in array
+ids = serial_connection.scan()
+
+#disable torque for servos
+for id in ids:
+    serial_connection.send(InstructionPacket(id, 0x03, bytes([0x18, 0x00])))
 
 #initialise position array
-position1 = []
-
+position = [[None for x in range(0)] for y in range(len(ids))]
+print(position)
 #initialise time
-init_time = time.time()
+start = time.time()
+
+def sure_goto(id, pos, sp):
+    max_retries = 10
+    retry = 0
+    for retry in range(max_retries):
+        try:
+            serial_connection.goto(id, pos, speed=sp)
+            break
+        except:
+            pass
+
 
 try:
     while True:
-       print("1")
+        for id in ids:
+            curr_pos = serial_connection.get_present_position(id)
+            position[id-1].append(curr_pos)
+            print(position[id-1])
+            time.sleep(0.05)
+
 except KeyboardInterrupt:
     pass
+
+for id in ids:
+    sure_goto(id, position[id-1][0], 200)
+
+time.sleep(1)
+
+for id in ids:
+    for i in range(len(position[1])):
+        sure_goto(id, position[id-1][i], 200)
+
+print("finished")
