0910106428ad2fca9c35e3fa0fca6973196eb400
tobiasdr <tobias.drees@gmail.com>
2019-05-26 19:25:07 +0100

    minor changes

--- app.py
+++ app.py
@@ -10,39 +10,49 @@ import rob
 app = Flask(__name__)
 # serial = None
 
-
+flag = 0
 @app.route("/")
 def main():
+    if flag == 1:
+        rob.record()
+    elif flag == 2:
+        rob.replay()
+    else:
+        pass
     return render_template('index.html')
 
 @app.route("/record", methods=['GET'])
 def record():
-    start = time.time()
-    while True:
-        for id in ids:
-            curr_pos = serial_connection.get_present_position(id)
-            position[id].append(curr_pos)
-            print(position[id])
-            time.sleep(0.08)
-            print(time.time()-start)
+    flag = 1
+    return
 
 
 @app.route("/replay", methods=['GET'])
 def replay():
-    for id in ids:
-        sure_goto(id, position[id][0], 200)
-        time.sleep(0.08)
-    time.sleep(1)
+    flag = 2
+    return
 
-    for id in ids:
-        for i in range(len(position[id])):
-            sure_goto(id, position[id][i], 300)
-            time.sleep(0.04)
-            print(time.time()-start)
 
 if __name__ == "__main__":
+    #initialise serial port S0
+    serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=True)
 
-    # serial = ...
-    t = threading.Thread(target=run)
+    ids = []
+    #scanning daisy chain and storing ID numbers in array
+    worked = False
+    while not worked:
+        try:
+            ids = serial_connection.scan()
+            worked = True
+        except:
+            pass
+
+    #disable torque for servos
+    for id in ids:
+        serial_connection.send(InstructionPacket(id, 0x03, bytes([0x18, 0x00])))
+
+    #initialise position array
+    position = [[None for x in range(0)] for y in range(12)]
+    t = threading.Thread(target=main)
     t.start()
     app.run(host="0.0.0.0", port=8080)
