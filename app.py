bb240eb8cb10fe3c3f4e2ae4d7f8f47cd6e23f47
tobiasdr <tobias.drees@gmail.com>
2019-05-26 19:03:01 +0100

    threading prototype

--- app.py
+++ app.py
@@ -3,52 +3,28 @@ from pyax12.instruction_packet import InstructionPacket
 import time
 import os
 from flask import Flask, render_template
+import threading
+import rob
 
-#function that ignores errors when moving
-def sure_goto(id, pos, sp):
-    max_retries = 10
-    retry = 0
-    for retry in range(max_retries):
-        try:
-            serial_connection.goto(id, pos, speed=sp)
-            break
-        except:
-            pass
-    print("Retries: {}".format(retry))
+
+app = Flask(__name__)
+# serial = None
+
 
 @app.route("/")
 def main():
-    #initialise serial port S0
-    serial_connection = Connection(port="/dev/ttyS0", baudrate=1000000, rpi_gpio=True)
-    ids = []
-    #scanning daisy chain and storing ID numbers in array
-    worked = False
-    while not worked:
-        try:
-            ids = serial_connection.scan()
-            worked = True
-        except:
-            pass
-
-    #disable torque for servos
-    for id in ids:
-        serial_connection.send(InstructionPacket(id, 0x03, bytes([0x18, 0x00])))
-
-    #initialise position array
-    position = [[None for x in range(0)] for y in range(7)]
     return render_template('index.html')
 
 @app.route("/record", methods=['GET'])
 def record():
-    try:
-        while True:
-            for id in ids:
-                curr_pos = serial_connection.get_present_position(id)
-                position[id].append(curr_pos)
-                print(position[id])
-                time.sleep(0.08)
-                print(time.time()-start)
-    return render_template('index.html')
+    start = time.time()
+    while True:
+        for id in ids:
+            curr_pos = serial_connection.get_present_position(id)
+            position[id].append(curr_pos)
+            print(position[id])
+            time.sleep(0.08)
+            print(time.time()-start)
 
 
 @app.route("/replay", methods=['GET'])
@@ -63,7 +39,10 @@ def replay():
             sure_goto(id, position[id][i], 300)
             time.sleep(0.04)
             print(time.time()-start)
-    return render_template('index.html')
 
 if __name__ == "__main__":
+
+    # serial = ...
+    t = threading.Thread(target=run)
+    t.start()
     app.run(host="0.0.0.0", port=8080)
---
+++ rob.py
@@ -0,0 +1,66 @@
+import threading
+from pyax12.connection import Connection
+from pyax12.instruction_packet import InstructionPacket
+import time
+import os
+from flask import Flask, render_template
+
+#function that ignores errors when moving
+def sure_goto(id, pos, sp):
+    max_retries = 10
+    retry = 0
+    for retry in range(max_retries):
+        try:
+            serial_connection.goto(id, pos, speed=sp)
+            break
+        except:
+            pass
+    print("Retries: {}".format(retry))
+
+
+FLAG_NO_ACTION = 0
+FLAG_RECORD = 1
+FLAG_REPLAY = 2
+
+flag = FLAG_NO_ACTION
+position = [[None for x in range(0)] for y in range(9)]
+
+def run():
+    while 1:
+        if flag == FLAG_NO_ACTION:
+            sleep(0.3)
+        elif flag == FLAG_RECORD:
+            record()
+        elif flag == FLAG_REPLAY:
+            replay()
+
+
+def record():
+    global position
+    position = [[None for x in range(0)] for y in range(9)]
+    while flag == FLAG_RECORD:
+        for id in ids:
+            curr_pos = serial_connection.get_present_position(id)
+            position[id].append(curr_pos)
+            print(position[id])
+            time.sleep(0.08)
+            print(time.time()-start) 
+
+
+def replay():
+    global position
+    move_counter = 0
+    while flag == FLAG_REPLAY and move_counter < len(moves):
+        for id in ids:
+            sure_goto(id, position[id][0], 200)
+            time.sleep(0.08)
+        time.sleep(1)
+
+        for id in ids:
+            for i in range(len(position[id])):
+                sure_goto(id, position[id][i], 300)
+                time.sleep(0.04)
+                print(time.time()-start)    
+    sleep(2)
+
+
